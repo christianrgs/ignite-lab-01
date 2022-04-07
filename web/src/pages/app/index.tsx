92abea86abb0dab50fbfd15f9efb41b38477c4e7
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-04-06 23:35:09 -0300

    initial structure of projects

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,20 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+
+export default function Home() {
+  const user = useUser();
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <Link href="/api/auth/logout">
+        <a>Logout</a>
+      </Link>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
